5a4b69ffbb30eb68b511fa713148dc0da8e4a10d
Ufuk Bakan <println.ufukbakan@gmail.com>
2023-07-11 01:35:38 +0300

    feat: add fake user

---
+++ test/mock-data/UserMock.ts
@@ -0,0 +1,29 @@
+import { User } from "../../src/common/services/UserService";
+
+// generated via faker
+
+const UserMock : User = {
+    id: 1,
+    address: {
+        city: "Port Kayleighville",
+        geo: {
+            lat: "-31.5099",
+            lng: "119.1913"
+        },
+        street: "Ericka Motorway",
+        suite: "South Dakota",
+        zipcode: "45372"
+    },
+    company: {
+        name: "Rempel - Bins",
+        bs: "unleash end-to-end experiences",
+        catchPhrase: "Configurable systematic project"
+    },
+    email: "Patsy_Moen72@hotmail.com",
+    name: "Wallace Funk",
+    username: "Freeda.Hilpert",
+    phone: "438.534.5423",
+    website: "http://colossal-merchant.net"
+}
+
+export default UserMock;
