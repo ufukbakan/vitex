11b966b94c44c140676dc53cac1eadbabd4842a5
Ufuk Bakan <println.ufukbakan@gmail.com>
2023-07-11 17:59:42 +0300

    feat: force to use threads

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
     test: {
         globals: true,
         environment: "jsdom",
+        threads: true,
         coverage: {
             provider: "istanbul",
             reporter: ["html", "json", "text"],
