8ed2f256011ef721eb8a93a4b00311d81e52d865
Ufuk Bakan <println.ufukbakan@gmail.com>
2023-07-11 10:20:58 +0300

    feat: add menu unit test

---
+++ test/unit/menu.unit.test.tsx
@@ -0,0 +1,31 @@
+import Menu from "../../src/common/components/Menu";
+import { renderWithRouter } from "../helpers";
+import { fireEvent } from "@testing-library/react";
+
+describe("Menu unit test", () => {
+
+    test("Home link test", () => {
+        const container = renderWithRouter(<Menu />);
+        const homeLink = container.queryByText("Home")!;
+        expect(homeLink).to.be.not.null;
+        fireEvent.click(homeLink);
+        expect(globalThis.location.pathname).to.be.eq("/");
+    });
+
+    test("About link test", () => {
+        const container = renderWithRouter(<Menu />);
+        const aboutLink = container.queryByText("About")!;
+        expect(aboutLink).to.be.not.null;
+        fireEvent.click(aboutLink);
+        expect(globalThis.location.pathname).to.be.eq("/about");
+    });
+
+    test("Profile link test", () => {
+        const container = renderWithRouter(<Menu />);
+        const profileLink = container.queryByText("Profile")!;
+        expect(profileLink).to.be.not.null;
+        fireEvent.click(profileLink);
+        expect(globalThis.location.pathname).to.be.eq("/profile/1");
+    });
+
+});
