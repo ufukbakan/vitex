1b7066bcb8ef1f8ba65df9ffec90f96db4979498
Ufuk Bakan <println.ufukbakan@gmail.com>
2023-07-12 01:17:27 +0300

    fix: axios mock & recoil root

--- test/unit/profile-card.unit.test.tsx
+++ test/unit/profile-card.unit.test.tsx
@@ -1,10 +1,11 @@
-import { render, waitFor } from "@testing-library/react";
+import { waitFor } from "@testing-library/react";
 import axios from "axios";
 import AxiosMock from "axios-mock-adapter";
 import { SpyInstance } from "vitest";
 import { userService } from "../../src/common/services/UserService";
 import Profile from "../../src/pages/profile/[id]";
 import * as Router from "../../src/router";
+import { renderWithProviders } from "../helpers";
 import UserMock from "../mock-data/UserMock";
 
 describe("Profile card unit test", () => {
@@ -20,35 +21,40 @@ describe("Profile card unit test", () => {
 
     beforeEach(() => {
         fetchUsersMock = vi.spyOn(userService, "fetchUserById");
-        axiosMock.onGet(/\/api\/users\/\d+/).reply(200, UserMock);
         useParamsMock = vi.spyOn(Router, "useParams");
         consoleErrorMock = vi.spyOn(console, "error");
     })
 
     afterEach(() => {
+        axiosMock.reset();
         fetchUsersMock.mockRestore();
-        axiosMock.restore();
         useParamsMock.mockRestore();
         consoleErrorMock.mockRestore();
     });
 
     test("Path error test", () => {
-        consoleErrorMock.mockImplementation(vi.fn(() => {}));
-        useParamsMock.mockReturnValueOnce({ id: "x" });
+        consoleErrorMock.mockImplementation(vi.fn(() => { }));
+        useParamsMock.mockReturnValue({ id: "x" });
 
-        const container = render(<Profile />);
+        const container = renderWithProviders(<Profile />);
         expect(container.queryByText("Id is not an integer")).to.be.not.null;
         expect(fetchUsersMock).not.toHaveBeenCalled();
     });
 
-    test("Load user information test", () => {
-        useParamsMock.mockReturnValueOnce({ id: "1" });
+    test("Load user information test", async () => {
+        useParamsMock.mockReturnValue({ id: "1" });
+        axiosMock.onGet("/api/users/1").reply(200, UserMock);
 
-        const container = render(<Profile />);
-        expect(container.queryByText("Loading", { exact: false })).to.be.not.null;
-        
-        waitFor(() => expect(fetchUsersMock).toHaveBeenCalledOnce(), { timeout: 2000 });
-        waitFor(() => expect(container.baseElement.innerHTML.includes("Username")).to.be.true);
+        const container = renderWithProviders(<Profile />);
+        const loadingDiv = container.queryByText("Loading", { exact: false });
+        expect(loadingDiv).to.be.not.null;
 
+        await waitFor(() => expect(fetchUsersMock).toHaveBeenCalledOnce(), { timeout: 2000 });
+
+        expect(container.queryByText(UserMock.id)).to.be.not.null;
+        expect(container.queryByText(UserMock.name)).to.be.not.null;
+        expect(container.queryByText(UserMock.username)).to.be.not.null;
+        expect(container.queryByText(UserMock.company.name)).to.be.not.null;
+        expect(container.queryByText(UserMock.address.city)).to.be.not.null;
     });
 });
